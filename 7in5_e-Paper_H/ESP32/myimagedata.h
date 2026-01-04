8e1b8a6eb76786925b6170509c4c5437d8c41738
Angus Eason <aje87@uclive.ac.nz>
2026-01-04 17:08:56 +0000

    Adding epaper H dev code, with wokring picture draw and write

---
+++ 7in5_e-Paper_H/ESP32/myimagedata.h
@@ -0,0 +1,11 @@
+#ifndef _MYIMAGEDATA_H_
+#define _MYIMAGEDATA_H_
+
+/* ------------------------------------------------ */
+// Imagedata
+extern const unsigned char myImage4color[];
+
+#endif
+/* FILE END */
+
+
